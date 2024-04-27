6d61c66554b3d44eb26514fc72a2e50f222dee8c
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-04-27 15:52:46 +0530

    code

---
+++ code-editor/app/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Resizable } from 'react-resizable';
+
+function App() {
+  const [width, setWidth] = React.useState(200);
+  const [height, setHeight] = React.useState(200);
+
+  return (
+    <Resizable
+      width={width}
+      height={height}
+      onResize={(event, { size }) => {
+        // Use 'element' if needed
+        setWidth(size.width);
+        setHeight(size.height);
+      }}
+    >
+      <div className="App">
+        <h1>Hello, world!</h1>
+      </div>
+    </Resizable>
+  );
+}
